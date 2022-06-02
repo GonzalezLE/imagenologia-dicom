ad1de3bee2c25db22badbadb371aa56edcbd4d0b
GonzalezLE <gonzalez-3216@hotmail.com>
2022-06-02 11:35:17 -0500

    Agrego documentacion y constante para el filtro de las carpetas

--- settings/settings.py
+++ settings/settings.py
@@ -1,1 +1,28 @@
+""" CARPETA_ADDRESS
+    this is an address to folder raiz
+"""
 CARPETA_ADDRESS = 'C:\imagenologia'
+
+""" FOLDER_UDN
+    This is a folder for the filter in the part of folder list
+"""
+FOLDER_UDN = [
+    'Acajete',
+    'Acatzingo',
+    'Cdserdan',
+    'Familydr',
+    'Grajales',
+    'Hospisana',
+    'Izucar',
+    'LA UNO',
+    'Libres',
+    'Lomabonita',
+    'Matriz',
+    'Snpedro',
+    'Stamaria',
+    'Tecamachalco',
+    'Tepexi',
+    'Tuxtepec',
+    'UnidadMovil',
+    'Xicotepec'
+]
